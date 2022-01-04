1749758e96727375137218c94571031695f6499c
Richard Arthur <richardarthur228@gmail.com>
2022-01-04 12:57:52 +0000

    updated useState

--- useState/src/App.js
+++ useState/src/App.js
@@ -1,7 +1,32 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [state, setState] = useState(0);
+
+  const handleDecrease = () => {
+    setState(state - 1);
+  };
+
+  const handleIecrease = () => {
+    setState(state + 1);
+  };
+
+  return (
+    <div className="app">
+      <h2>useState Boilerplate</h2>
+
+      <div className="content">
+        <button onClick={handleDecrease} className="decrease">
+          decrease
+        </button>
+        <p>{state}</p>
+        <button onClick={handleIecrease} className="increase">
+          increase
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
